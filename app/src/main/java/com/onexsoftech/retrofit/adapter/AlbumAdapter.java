06567a6211e49156274036ad558ec8aaf37e1bdd
pallavi <naramsettypallavi@gmail.com>
2019-04-30 14:21:00 +0530

    added project

--- app/src/main/java/com/onexsoftech/retrofit/adapter/AlbumAdapter.java
+++ app/src/main/java/com/onexsoftech/retrofit/adapter/AlbumAdapter.java
@@ -2,22 +2,17 @@ package com.onexsoftech.retrofit.adapter;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.DividerItemDecoration;
-import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.onexsoftech.retrofit.R;
 import com.onexsoftech.retrofit.Result;
-import com.onexsoftech.retrofit.TextAdapter;
 
 
 import java.util.ArrayList;
